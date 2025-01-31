5667a5dbc6303f0821d34d4cb2a0b8aad71b251e
Judy <158211720+judyyan27@users.noreply.github.com>
2025-01-31 12:30:09 -0500

    Ice Cream scoop project

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 [![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-2972f46106e565e64193e422d61a12cf1da4916b45550586e14ef0a7c637dd04.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=17740706)
 
 Project Idea: Dessert themed game or customization webpage
+
+## Ice Cream Cone Customizer--
+
+### The website will consist of a product container of all the available ice cream scoop flavors (vanilla, chocolate, strawberry, etc..)
+
+-- Top Section contains the header
+-- A section that was the list of flavors, an image, price, and button to add to cone
+-- A section that contains the ice cream and a button to remove a scoop, reset, etc
+-- A section with the total price and list of flavors
---
+++ app/src/components/Card.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h2>{{ animal.name }}</h2>
+    <img src="animal.image" alt="" />
+  </div>
+</template>
+
+<script setup>
+// Define props that the component will receive
+defineProps({
+  name: String,
+  price: Number,
+  image: String,
+})
+</script>
+
+<style scoped></style>
+
+<!-- <template>
+  <div class="flex flex-wrap gap-4 justify-center">
+    <IceCreamCard
+      v-for="iceCream in iceCreams"
+      :key="iceCream.name"
+      :name="iceCream.name"
+      :price="iceCream.price"
+      :image="iceCream.image"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import IceCreamCard from "./components/IceCreamCard.vue";
+
+// Sample ice cream data
+const iceCreams = ref([
+  { name: "Vanilla", price: 2.99, image: "https://via.placeholder.com/100?text=Vanilla" },
+  { name: "Chocolate", price: 3.49, image: "https://via.placeholder.com/100?text=Chocolate" },
+  { name: "Strawberry", price: 3.29, image: "https://via.placeholder.com/100?text=Strawberry" },
+  { name: "Mint Chip", price: 3.59, image: "https://via.placeholder.com/100?text=Mint+Chip" }
+]);
+</script>
+-->
