28f2f8b7a65230908ee63d3b22f338f94866c5ae
Judy <judyy10@nycstudents.net>
2025-02-23 21:05:22 -0500

    Update IceCreamList.vue

--- app/src/components/IceCreamList.vue
+++ app/src/components/IceCreamList.vue
@@ -106,110 +106,6 @@ onUpdated(() => {
 })
 </script>
 
-<!-- <template>
-  <div class="card-container">
-    <IceCreamCard
-      v-for="item in shop"
-      :name="item.name"
-      :price="item.price"
-      :image="item.image"
-      :index="item.index"
-      v-on:AddtoCart="addingitem"
-    />
-  </div>
-  <div class="cart">
-    <h2>Shopping Cart:</h2>
-    <h2>-----------------</h2>
-    <ul>
-      <li v-for="(item, index) in cart" :key="index">
-        <span> {{ item.name }} - ${{ item.price }} </span>
-        <button @click="deleteItem(index)">Remove Item</button>
-      </li>
-    </ul>
-    <h3>Total Cost: ${{ price }}</h3>
-  </div>
-</template>
-
-<script setup>
-import IceCreamCard from './IceCreamCard.vue'
-import { onUpdated, ref } from 'vue'
-
-defineProps({
-  cart: Array,
-})
-
-const cart = ref([])
-
-function addingitem(index) {
-  console.log(index, '- Adding item to cart')
-  const item = shop.value[index] // Get the item from the shop array
-  if (item) {
-    cart.value.push({ name: item.name, price: item.price }) // Append item to cart
-    console.log(cart.value) // Console.log updated cart
-  }
-}
-
-let shop = ref([
-  {
-    name: 'Vanilla',
-    price: 1.99,
-    image: 'vanilla_icecream.png',
-    index: 0,
-  },
-  {
-    name: 'Chocolate',
-    price: 2.0,
-    image: 'chocolate.png',
-    index: 1,
-  },
-  {
-    name: 'Strawberry',
-    price: 2.0,
-    image: 'strawberry.png',
-    index: 2,
-  },
-  {
-    name: 'Cookies and Cream',
-    price: 2.5,
-    image: 'cookiesncream.png',
-    index: 3,
-  },
-  {
-    name: 'Mint Chocolate Chip',
-    price: 2.25,
-    image: 'mintchoco.png',
-    index: 4,
-  },
-  {
-    name: 'Chocolate Chip',
-    price: 2.0,
-    image: 'choco_chip.webp',
-    index: 5,
-  },
-  {
-    name: 'Coffee',
-    price: 2.25,
-    image: 'coffee.png',
-    index: 6,
-  },
-  {
-    name: 'Neapolitan',
-    price: 2.5,
-    image: 'neo.png',
-    index: 7,
-  },
-])
-
-const price = ref('0.00')
-
-function displayCart() {}
-
-const deleteItem = (index) => {
-  console.log(index, '-delete item function')
-  item.value.splice(index, 1)
-}
-</script> -->
-
 <style scoped>
 .card-container {
   width: 80%;
@@ -243,7 +139,7 @@ p {
 }
 
 .cart {
-  height: 140rem;
+  height: fit-content;
   width: 25%;
   background-color: azure;
   padding: 0.5rem;
